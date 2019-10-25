86204740742c418a709f6bc0889a2c24bd6fa999
Zhitao Zou <zhitaozou97@gmail.com>
2019-10-25 14:11:56 +0800

    kmeans++ hw2 p2

--- hw2/kmeans.py
+++ hw2/kmeans.py
@@ -22,4 +22,21 @@ def kmeans(data:np.array,MAX_ITER:int, init_point:np.array,dis_fun=L2):
 
     return cost,init_point
 
+def kmeanspp(data:np.array,k:int,MAX_ITER:int,dis_fun=L2):
+    """
+    kmeans++: https://en.wikipedia.org/wiki/K-means%2B%2B
+    """
+    
+    init_point = np.zeros((k,data.shape[1]))
+    init_point[0] = data[np.random.choice(data.shape[0])]
+    for i in range(1,k):
+        p = [np.min([dis_fun(point,cluster)for cluster in init_point]) for point in data]
+        assert len(p) == data.shape[0]
+        p/=sum(p)
+        next_cluster = np.random.choice(len(p),1,p=p)
+        init_point[i] = data[next_cluster]
+
+        
+    assert init_point.shape[0] == k and init_point.shape[1] == data.shape[1],"{}".format(init_point.shape) 
+    return kmeans(data,MAX_ITER,init_point,dis_fun)    
     
--- hw2/p2.py
+++ hw2/p2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from kmeans import kmeans
+from kmeans import kmeans,kmeanspp
 from distance import L2,mahaten
 
 
@@ -19,13 +19,18 @@ c2 = pd.read_csv(INIT_2,' ',header=None).values
 
 MAX_ITER = 100
 
-dis_fun = mahaten
+dis_fun = L2
 
+N_CLUSTER = 10
 
-cost1,center1 = kmeans(data,20,c1,dis_fun)
-cost2,center2 = kmeans(data,20,c2,dis_fun)
+cost3,center3 = kmeanspp(data,N_CLUSTER,MAX_ITER,dis_fun)
+
+cost1,center1 = kmeans(data,MAX_ITER,c1,dis_fun)
+cost2,center2 = kmeans(data,MAX_ITER,c2,dis_fun)
+
 
 plt.plot(cost1,label='c1')
 plt.plot(cost2,label='c2')
+plt.plot(cost3,label='c3')
 plt.legend()
 plt.show()
